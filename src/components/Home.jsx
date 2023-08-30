2572846118885e71ddf575389ae194befb7a4dfa
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-08-30 23:32:25 +0400

    adding components and addets

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
+      {/* container */}
+      <div className="max-w-[1000px] mx-auto px-8 flex-col justify-center h-full">
+        <p>Hi, my name is</p>
+        <h1>Levan Gabedava</h1>
+        <h2>I'm Front-end Developer</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
