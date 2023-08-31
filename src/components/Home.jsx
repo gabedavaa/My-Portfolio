f1656bef588702da36f660d6750c945cd3300aa9
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-08-31 23:39:10 +0400

    adding About Home and Skills files

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,32 @@
 import React from "react";
+import { HiArrowCircleRight, HiArrowNarrowRight } from "react-icons/hi";
 
 const Home = () => {
   return (
     <div name="home" className="w-full h-screen bg-[#0a192f]">
       {/* container */}
-      <div className="max-w-[1000px] mx-auto px-8 flex-col justify-center h-full">
-        <p>Hi, my name is</p>
-        <h1>Levan Gabedava</h1>
-        <h2>I'm Front-end Developer</h2>
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-pink-600">Hi, my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Levan Gabedava
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          I'm a Front-end Developer
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          Self-taught front-end developer with a strong motivationand passion
+          for coding. Proven ability to quickly learn newskills through online
+          resources and mentorship. Efficientlyhandle multiple tasks and thrive
+          on challenges. Eager tocontribute to a dynamic organization's success.
+        </p>
+        <div>
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+            View Work
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3" />
+            </span>
+          </button>
+        </div>
       </div>
     </div>
   );
